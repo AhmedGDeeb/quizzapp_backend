f7f42f3ccfbc4d4db0cb3dbb1a5f7819a620e949
AhmadGDeeb <khamideem@gmail.com>
2025-08-28 13:26:47 +0300

    [API-Versioning] add 'X-Api-Version' for reponse header

--- quizzapp/middleware/versioning.py
+++ quizzapp/middleware/versioning.py
@@ -1,5 +1,22 @@
 from django.utils.deprecation import MiddlewareMixin
 
 class VersioningMiddleware(MiddlewareMixin):
-    # TODO: Add Versioning API middleware
-    pass
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        print(request.headers)
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+        
+        # Add supported version
+        response['X-Api-Version'] = '0.0'
+        
+        return response
