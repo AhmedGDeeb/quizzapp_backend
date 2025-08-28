424f721cd56bd6939482e335c66bebfd002cd0c7
AhmadGDeeb <khamideem@gmail.com>
2025-08-28 12:01:34 +0300

    dev@loc: [API-Versioning] blueprint for code and unit-tests

---
+++ quizzapp/middleware/versioning.py
@@ -0,0 +1,5 @@
+from django.utils.deprecation import MiddlewareMixin
+
+class VersioningMiddleware(MiddlewareMixin):
+    # TODO: Add Versioning API middleware
+    pass
---
+++ test/test_versioning.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from quizzapp.middleware.versioning import *
+
+class SomeTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
